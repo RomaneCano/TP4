231b1ba7ecf3b2fb2ab91bc32be174fa00383bae
Cano <romane.cano@etud.univ-jfc.fr>
2024-12-04 18:47:35 +0100

    commit test

--- src/main/java/org/github/bastide/Main.java
+++ src/main/java/org/github/bastide/Main.java
@@ -6,5 +6,5 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Bonjour, bienvenue sur TP4 !"); //texte affiché à l'utilisateur
-        }
+        } //
     }
